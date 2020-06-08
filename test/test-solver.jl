4a44f0cd58c79ceb00c5b751e67e34d52186a171
luke-govia <lukegovia4@gmail.com>
2020-06-08 16:50:59 -0400

    Fix ME solver testing

--- test/test-solver.jl
+++ test/test-solver.jl
@@ -21,11 +21,16 @@ function test_solver_args()
 	tvec2, rho_out2 = MESolve.me_solve_time_independent(rho_in, H, gamma, rates, t0, tf, tstep = tstep, tols = tols, alg = DP5())
 
 	# test time dependent solvers
-	Ht(t) = exp(1im*t)*H
+	Ht(t) = cos(t)*H
+
+	function H_ip(Htemp::AbstractArray,t::AbstractFloat)
+		Htemp[:,:] = H(t)[:,:]
+		nothing
+	end
 	rt(t) = exp(-t)*rates
 
-	tvec_t1, rho_out_t1 = MESolve.me_solve_H_time_dependent(rho_in, Ht, gamma, rates, t0, tf, tstep = tstep, tols = tols)
-	tvec_t2, rho_out_t2 = MESolve.me_solve_H_time_dependent(rho_in, Ht, gamma, rates, t0, tf, tstep = tstep, tols = tols, alg = DP5())
+	tvec_t1, rho_out_t1 = MESolve.me_solve_H_time_dependent(rho_in, H_ip, gamma, rates, t0, tf, tstep = tstep, tols = tols)
+	tvec_t2, rho_out_t2 = MESolve.me_solve_H_time_dependent(rho_in, H_ip, gamma, rates, t0, tf, tstep = tstep, tols = tols, alg = DP5())
 	tvec_t3, rho_out_t3 = MESolve.me_solve_L_time_dependent(rho_in, H, gamma, rt, t0, tf, tstep = tstep, tols = tols)
 	tvec_t4, rho_out_t4 = MESolve.me_solve_L_time_dependent(rho_in, H, gamma, rt, t0, tf, tstep = tstep, tols = tols, alg = DP5())
 	tvec_t5, rho_out_t6 = MESolve.me_solve_full_time_dependent(rho_in, Ht, gamma, rt, t0, tf, tstep = tstep, tols = tols)
